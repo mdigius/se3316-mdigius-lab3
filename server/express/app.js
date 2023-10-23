240e2ad788fd96b26a8a10b33894803e770d4e37
Michael Di Giuseppe <mdigius@uwo.ca>
2023-10-23 12:23:35 -0400

    Fixed power search and added search by race.

--- server/express/app.js
+++ server/express/app.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const app = express()
 
-
+// Parse the provided JSON files to be used by app object
 const superheroInfo = JSON.parse(fs.readFileSync('superhero_info.json'));
 const superheroPowers = JSON.parse(fs.readFileSync('superhero_powers.json'));
 app.use(express.json())
@@ -35,8 +35,8 @@ app.route('/api/superheroInfo/name/:name')
             res.status(404).json({ message: `No existing superhero with name: ${name}` });
         }
     })
-// Functionality for searching powers by name
-app.route('/api/powers/:name')
+// Functionality for searching powers for a given superhero name
+app.route('/api/powers/hero/:name')
     .get((req, res) => {
         const name = req.params.name;
         const powers = superheroPowers.find((power) => power.hero_names == name);
@@ -46,6 +46,23 @@ app.route('/api/powers/:name')
             res.status(404).json({ message: `No existing powers for superhero with name: ${name}` });
         }
     })
+// Search functionality to return all hero names with a given power
+app.get('/api/powers/:power', (req, res) => {
+    const power = req.params.power;
+    const heroes = new Set()
+    superheroPowers.forEach((hero) => {
+        if(hero[power] == "True"){
+            heroes.add(hero.hero_names)
+        }
+    })
+    if (heroes.size > 0) {
+        res.json(Array.from(heroes));
+    } else {
+        res.status(404).json({ message: `No existing heroes for power: ${power}` });
+    }
+
+})
+
 // Returns list of all publishers
 app.get('/api/publishers', (req, res) => {
     // Stores unique publishers in a set
@@ -65,10 +82,35 @@ app.get('/api/publishers/:id', (req, res) => {
             heroes.add(hero)
         }
     })
-    if (heroes) {
+    if (heroes.size > 0) {
         res.json(Array.from(heroes));
     } else {
         res.status(404).json({ message: `No existing heroes for publisher: ${pub}` });
+    }
+})
+// Return list of all races
+app.get('/api/race', (req, res) => {
+    // Stores unique publishers in a set
+    const races = new Set(); 
+    superheroInfo.forEach((hero) => {
+    races.add(hero.Race);
+    });
+    // Sends response as array
+    res.json(Array.from(races)); 
+    });
+// Returns a list of all hero names from a given race
+app.get('/api/race/:id', (req, res) => {
+    const race = req.params.id;
+    const heroes = new Set()
+    superheroInfo.forEach((hero) => {
+        if(hero.Race == race){
+            heroes.add(hero.name)
+        }
+    })
+    if (heroes.size > 0) {
+        res.json(Array.from(heroes));
+    } else {
+        res.status(404).json({ message: `No existing heroes for race: ${race}` });
     }
 })
 
