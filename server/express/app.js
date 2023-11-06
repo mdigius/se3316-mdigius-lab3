69621b31a0f81f31426e2832508cf367a81145d1
Michael Di Giuseppe <mdigius@uwo.ca>
2023-11-06 15:21:15 -0500

    Added functionality to ignore capitals in API

--- server/express/app.js
+++ server/express/app.js
@@ -1,122 +1,121 @@
-// Require express and filesystem modules
 const express = require('express');
-const fs = require('fs')
-const cors = require('cors'); // Import the cors package
+const fs = require('fs');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
+app.use(cors());
 
-app.use(cors()); // Enable CORS for all routes
-// Parse the provided JSON files to be used by app object
 const superheroInfo = JSON.parse(fs.readFileSync('superhero_info.json'));
 const superheroPowers = JSON.parse(fs.readFileSync('superhero_powers.json'));
-app.use(express.json())
-// Returns json objects of all superheroes
+app.use(express.json());
+
+// Returns JSON objects of all superheroes
 app.route('/api/superheroInfo')
     .get((req, res) => {
-        res.json(superheroInfo)
-    })
+        res.json(superheroInfo);
+    });
+
 // Functionality for searching by superheroID
 app.route('/api/superheroInfo/:id')
     .get((req, res) => {
         const superheroId = parseInt(req.params.id);
-        const superhero = superheroInfo.find((hero) => hero.id == superheroId);
+        const superhero = superheroInfo.find((hero) => hero.id === superheroId);
         if (superhero) {
             res.json(superhero);
         } else {
             res.status(404).json({ message: `No existing superhero with id: ${superheroId}` });
         }
-    })
+    });
+
 // Functionality for searching by name
 app.route('/api/superheroInfo/name/:name')
     .get((req, res) => {
-        const name = req.params.name;
-        const superhero = superheroInfo.find((hero) => hero.name == name);
+        const name = req.params.name.toLowerCase();
+        const superhero = superheroInfo.find((hero) => hero.name.toLowerCase() === name);
         if (superhero) {
             res.json(superhero);
         } else {
             res.status(404).json({ message: `No existing superhero with name: ${name}` });
         }
-    })
+    });
+
 // Functionality for searching powers for a given superhero name
 app.route('/api/powers/hero/:name')
     .get((req, res) => {
-        const name = req.params.name;
-        const powers = superheroPowers.find((power) => power.hero_names == name);
+        const name = req.params.name.toLowerCase();
+        const powers = superheroPowers.find((power) => power.hero_names.toLowerCase() === name);
         if (powers) {
             res.json(powers);
         } else {
             res.status(404).json({ message: `No existing powers for superhero with name: ${name}` });
         }
-    })
+    });
+
 // Search functionality to return all hero names with a given power
 app.get('/api/powers/:power', (req, res) => {
-    const power = req.params.power;
-    const heroes = new Set()
+    const power = req.params.power.toLowerCase();
+    const heroes = new Set();
     superheroPowers.forEach((hero) => {
-        if(hero[power] == "True"){
-            heroes.add(hero.hero_names)
+        if (hero[power] === "true") {
+            heroes.add(hero.hero_names);
         }
-    })
+    });
     if (heroes.size > 0) {
         res.json(Array.from(heroes));
     } else {
         res.status(404).json({ message: `No existing heroes for power: ${power}` });
     }
-
-})
+});
 
 // Returns list of all publishers
 app.get('/api/publishers', (req, res) => {
-    // Stores unique publishers in a set
-    const publishers = new Set(); 
+    const publishers = new Set();
     superheroInfo.forEach((hero) => {
-    publishers.add(hero.Publisher);
+        publishers.add(hero.Publisher.toLowerCase());
     });
-    // Sends response as array
-    res.json(Array.from(publishers)); 
-    });
+    res.json(Array.from(publishers));
+});
+
 // Returns a list of all heroes from a given publisher
 app.get('/api/publishers/:id', (req, res) => {
-    const pub = req.params.id;
-    const heroes = new Set()
+    const pub = req.params.id.toLowerCase();
+    const heroes = new Set();
     superheroInfo.forEach((hero) => {
-        if(hero.Publisher == pub){
-            heroes.add(hero)
+        if (hero.Publisher.toLowerCase() === pub) {
+            heroes.add(hero);
         }
-    })
+    });
     if (heroes.size > 0) {
         res.json(Array.from(heroes));
     } else {
         res.status(404).json({ message: `No existing heroes for publisher: ${pub}` });
     }
-})
+});
+
 // Return list of all races
 app.get('/api/race', (req, res) => {
-    // Stores unique publishers in a set
-    const races = new Set(); 
+    const races = new Set();
     superheroInfo.forEach((hero) => {
-    races.add(hero.Race);
+        races.add(hero.Race.toLowerCase());
     });
-    // Sends response as array
-    res.json(Array.from(races)); 
-    });
+    res.json(Array.from(races));
+});
+
 // Returns a list of all hero names from a given race
 app.get('/api/race/:id', (req, res) => {
-    const race = req.params.id;
-    const heroes = new Set()
+    const race = req.params.id.toLowerCase();
+    const heroes = new Set();
     superheroInfo.forEach((hero) => {
-        if(hero.Race == race){
-            heroes.add(hero.name)
+        if (hero.Race.toLowerCase() === race) {
+            heroes.add(hero.name);
         }
-    })
+    });
     if (heroes.size > 0) {
         res.json(Array.from(heroes));
     } else {
         res.status(404).json({ message: `No existing heroes for race: ${race}` });
     }
-})
+});
 
-
-
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(3000, () => console.log('Listening on port 3000'));
